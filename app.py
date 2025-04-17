373260e7a38e086ad28cb220137111700569f4ff
MabelHuang5 <huangmabel59@gmail.com>
2025-04-17 13:15:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import plotly.graph_objects as go
 
 # =============================================================================
 # Session State: Initialize an empty DataFrame to store aggregated monthly data
